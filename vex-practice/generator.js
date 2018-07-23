6d8e5d030efc8a87c49b7d66e3bd507ab478d781
Andrew Brogan <youdoof@gmail.com>
2018-07-22 22:23:14 -0500

    Vex Practice 5
    Started formatting the webpage that these svg's are being drawn on. More responsive to different screen sizes, but more hurdles to jump over down the road. Got triplets working with less effort than I originally thought was needed.

--- vex-practice/generator.js
+++ vex-practice/generator.js
@@ -5,25 +5,34 @@ function createNote(duration) {
 	return new VF.StaveNote({keys: ["c/5"], duration: duration});
 }
 
-// addArticulation(0, addAccent())
+// .addArticulation(0, addAccent())
 function addAccent() {
 	return new VF.Articulation("a>").setPosition(3);
 }
 
-// addModifier(0, addGraceNote())
+// .addModifier(0, addGraceNote())
 function addGraceNote() {
 	var gracenote = new VF.GraceNote({keys: ["c/5"], duration: "8", slash: true });
 	return new VF.GraceNoteGroup([gracenote]);
 }
 
-// 
+// Create 4 new StaveNotes
 function sixteenth() {
 	return [
 		createNote("16"),
 		createNote("16"),
 		createNote("16"),
 		createNote("16")
-	]
+	];
+}
+
+// Create 3 new StaveNotes
+function triplet() {
+	return [
+		createNote("8"),
+		createNote("8"),
+		createNote("8")
+	];
 }
 
 // Create first four counts of the grid, one variation at a time
@@ -31,7 +40,7 @@ function getFourCounts() {
 
 }
 
-// Create 
+// Not sure if this will work yet...
 function getTwoCounts() {
 
 }
--- vex-practice/vex-practice-5.js
+++ vex-practice/vex-practice-5.js
@@ -9,12 +9,12 @@ var MEASURE_ONE = OFFSET;
 var MEASURE_TWO = MEASURE_SIZE + OFFSET;
 var MEASURE_THREE = MEASURE_SIZE * 2 + OFFSET;
 
-// Create SVG renderer and attach it to the div element named "bar"
-var div = document.getElementById("bar");
+// Create SVG renderer and attach it to the div element named "renderSpace"
+var div = document.querySelector("#renderSpace");
 var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Size our svg
-renderer.resize(1600, 300);
+renderer.resize(1520, 300);
 
 // Get drawing context
 var context = renderer.getContext();
@@ -40,42 +40,50 @@ stave11.setContext(context).draw();
 stave12.setContext(context).draw();
 stave13.setContext(context).draw();
 
-// 16th >e&a
-var d00 = [
-	createNote("16").addArticulation(0, addAccent()),
-	createNote("16"),
-	createNote("16"),
-	createNote("16")
-];
-
-// 16th 1>&a
-var d01 = [
-	createNote("16"),
-	createNote("16").addArticulation(0, addAccent()),
-	createNote("16"),
-	createNote("16")
-];
-
-// 16th 1e>a
-var d02 = [
-	createNote("16"),
-	createNote("16"),
-	createNote("16").addArticulation(0, addAccent()),
-	createNote("16")
-];
-
-// 16th 1e&>
-var d03 = [
-	createNote("16"),
-	createNote("16"),
-	createNote("16"),
-	createNote("16").addArticulation(0, addAccent())
-];
-
-// Measure 1
-var m01 = sixteenth();
+/*
+	Measure 1, 16th notes
+*/
+// Populate 4 counts of 16th notes
+var m01 = sixteenth().concat(sixteenth()).concat(sixteenth()).concat(sixteenth());
+// Setup Beams and Stem Directions
 var b01 = VF.Beam.generateBeams(m01, {stem_direction: 1});
+// Format and Draw StaveNotes
 VF.Formatter.FormatAndDraw(context, stave01, m01);
+// Draw beams
 b01.forEach(function(b) {b.setContext(context).draw()});
 
-// Measure 2
+/*
+	Measure 2, Triplets
+*/
+// Populate 4 counts of triplets
+var m02 = triplet().concat(triplet()).concat(triplet()).concat(triplet());
+
+// Setup tuplets
+var tuplets = [
+	[0,3],
+	[3,6],
+	[6,9],
+	[9,12]
+].map(function(i){
+	return new VF.Tuplet(m02.slice(i[0], i[1]));
+});
+
+// Setup Beams
+var b02 = VF.Beam.generateBeams(m02, {stem_direction: 1});
+
+VF.Formatter.FormatAndDraw(context, stave02, m02);
+
+// Draw beams
+b02.forEach(function(b) {b.setContext(context).draw()});
+
+// Draw tuplets
+tuplets.forEach(function(t){
+	t.setContext(context).draw();
+});
+
+
+// This code coupled with css entry for SVG makes the svg scale and respond to the
+// webpage changing size and shape
+var svg = document.querySelector('svg');
+// Adding a viewBox attribute with the width and height same as the size of the file
+svg.setAttribute("viewBox", "0 0 1520 300");
