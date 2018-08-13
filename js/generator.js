4115b5fb1d39c5df6e3a86f7ba5ebc750ce94793
Andrew Brogan <youdoof@gmail.com>
2018-08-13 16:01:16 -0500

    Tweaking UI, Recycle SVG, Begin Modularize Drawing
    Tried messing with margins to get some room on mobile, but to not avail for now. Started getting setup to clear and redraw new measures when new parameters are set. That's gonna be a doozy for a while.

--- js/generator.js
+++ js/generator.js
@@ -1,5 +1,14 @@
 var VF = Vex.Flow;
 
+// Constants for stave size and locations
+var MEASURE_WIDTH = 600;
+var MEASURE_HEIGHT = 90;
+
+// Bar Lengths
+var EIGHT_BARS = 8;
+var NINE_BARS = 9;
+var FOUR_BARS = 4;
+
 // Partials for creating beats.
 var NONE = -1;
 var FIRST_PARTIAL = 0;
@@ -7,15 +16,61 @@ var SECOND_PARTIAL = 1;
 var THIRD_PARTIAL = 2;
 var FOURTH_PARTIAL = 3;
 
+// Rhythm Constants
 var SIXTEENTH = 0;
 var TRIPLET = 1;
 var EIGHTH = 2;
 
-function contextMaker(divID) {
+function createContexts(number) {
+	var contexts = [];
+	for (i = 1; i <= number; i++) {
+		var temp = new VF.Renderer(
+			document.querySelector(`#measure${i}`),
+			VF.Renderer.Backends.SVG
+		);
+		temp.resize(MEASURE_WIDTH, MEASURE_HEIGHT);
+		contexts.push(temp);
+	}
+	return contexts;
+}
+
+function createContext(divID) {
 	var d = document.querySelector(divID);
 	var r = new VF.Renderer(d, VF.Renderer.Backends.SVG);
-	r.resize(MEASURE_WIDTH, MEASURE_HEIGHT);
+	if (divID === "#measure9") {
+		r.resize(MEAS_WIDTH_TRIP, MEASURE_HEIGHT);
+	} else {
+		r.resize(MEASURE_WIDTH, MEASURE_HEIGHT);
+	}
 	return r.getContext();
+}
+
+function createStaves(number) {
+	var staves = [];
+	for (var i = 0; i < number; i++) {
+		staves.push(new VF.Stave(0, 0, MEASURE_WIDTH));
+	}
+	return staves;
+}
+
+function createRepeats(staves) {
+	if (staves.length === EIGHT_BARS) {
+		staves[4].setBegBarType(VF.Barline.type.REPEAT_BEGIN);
+		staves[5].setEndBarType(VF.Barline.type.REPEAT_END);
+		staves[6].setBegBarType(VF.Barline.type.REPEAT_BEGIN);
+		staves[7].setEndBarType(VF.Barline.type.REPEAT_END);
+	} else if (staves.length === FOUR_BARS) {
+		for (var i = 0; i < staves.length; i++) {
+			staves[i].setBegBarType(VF.Barline.type.REPEAT_BEGIN);
+			staves[i].setEndBarType(VF.Barline.type.REPEAT_END);
+		}
+	}
+}
+
+function setContextAndDraw(obj, ctx) {
+	for (var i = 0; i < obj.length; i++) {
+		obj[i].setContext(ctx[i]).draw();
+	}
 }
 
 // new VF.StaveNote({keys: ["c/5"], duration: "16"})
@@ -61,13 +116,13 @@ function getBeat(obj) {
 	if (obj.flam !== -1) {
 		notes[obj.flam].addModifier(0, addFlam());
 	}
-	
+
 	if (obj.accent !== -1) {
 		notes[obj.accent].addArticulation(0, addAccent());
 	}
 
 	if (obj.drag !== -1) {
-		notes[obj.drag].addArticulation(0, new VF.Tremolo(1).setYShift(-100));
+		notes[obj.drag].addArticulation(0, new VF.Tremolo(1));
 	}
 
 	return notes;
@@ -103,5 +158,3 @@ function fillBar(obj, mods) {
 	}
 	return bars;
 }
-
-
--- js/interactor.js
+++ js/interactor.js
@@ -1,19 +1,28 @@
 var VF = Vex.Flow;
 
 // Constants for stave size and locations
+// 16TH NOTE
 var MEASURE_WIDTH = 600;
 var MEASURE_HEIGHT = 90;
 
+// triplet
+var MEAS_WIDTH_TRIP = 450;
+
 // Get drawing context
-var context1 = contextMaker("#measure1");
-var context2 = contextMaker("#measure2");
-var context3 = contextMaker("#measure3");
-var context4 = contextMaker("#measure4");
-var context5 = contextMaker("#measure5");
-var context6 = contextMaker("#measure6");
-var context7 = contextMaker("#measure7");
-var context8 = contextMaker("#measure8");
-// var context9 = contextMaker("#measure9");
+
+// var contexts = createContexts(EIGHT_BARS);
+
+var context1 = createContext("#measure1");
+var context2 = createContext("#measure2");
+var context3 = createContext("#measure3");
+var context4 = createContext("#measure4");
+var context5 = createContext("#measure5");
+var context6 = createContext("#measure6");
+var context7 = createContext("#measure7");
+var context8 = createContext("#measure8");
+var context9 = createContext("#measure9");
+
+// var staves = createStaves(EIGHT_BARS);
 
 // Create a stave of width 90 on the canvas
 var stave01 = new VF.Stave(0, 0, MEASURE_WIDTH);
@@ -24,7 +33,7 @@ var stave05 = new VF.Stave(0, 0, MEASURE_WIDTH);
 var stave06 = new VF.Stave(0, 0, MEASURE_WIDTH);
 var stave07 = new VF.Stave(0, 0, MEASURE_WIDTH);
 var stave08 = new VF.Stave(0, 0, MEASURE_WIDTH);
-// var stave09 = new VF.Stave(0, 0, MEASURE_WIDTH);
+var stave09 = new VF.Stave(0, 0, MEAS_WIDTH_TRIP);
 
 // Repeats ?
 stave05.setBegBarType(VF.Barline.type.REPEAT_BEGIN);
@@ -32,10 +41,7 @@ stave06.setEndBarType(VF.Barline.type.REPEAT_END);
 stave07.setBegBarType(VF.Barline.type.REPEAT_BEGIN);
 stave08.setEndBarType(VF.Barline.type.REPEAT_END);
 
-// Add clefs and time signatures
-// stave01.addClef("percussion").addTimeSignature("4/4");
-// stave04.addClef("percussion").addTimeSignature("4/4");
-// stave07.addClef("percussion").addTimeSignature("4/4");
+// createRepeats(staves);
 
 // Connect staves to the rendering context and draw
 stave01.setContext(context1).draw();
@@ -46,7 +52,9 @@ stave05.setContext(context5).draw();
 stave06.setContext(context6).draw();
 stave07.setContext(context7).draw();
 stave08.setContext(context8).draw();
-// stave09.setContext(context9).draw();
+stave09.setContext(context9).draw();
+
+// setContextAndDraw(staves, contexts);
 
 /*
 	These variations should be customizable by the GUI on the webpage.
@@ -84,6 +92,13 @@ var variation4 = {
 	drag: FIRST_PARTIAL
 }
 
+var trip1 = {
+	rhythm: TRIPLET,
+	accent: SECOND_PARTIAL,
+	flam: NONE,
+	drag: SECOND_PARTIAL
+}
+
 /*
 	MEASURE 01
 */
@@ -91,6 +106,7 @@ var m01 = getFourCounts(variation1);
 var b01 = VF.Beam.generateBeams(m01, {stem_direction: 1});
 VF.Formatter.FormatAndDraw(context1, stave01, m01);
 b01.forEach(function(b) {b.setContext(context1).draw()});
+// setContextAndDraw(b01, contexts[0])
 
 /*
 	MEASURE 02
@@ -154,6 +170,32 @@ var b08 = VF.Beam.generateBeams(m08, {stem_direction: 1});
 VF.Formatter.FormatAndDraw(context8, stave08, m08);
 b08.forEach(function(b) {b.setContext(context8).draw()});
 
+/*
+	MEASURE 09
+	TRIPLET TOWN
+*/
+
+var m09 = getFourCounts(trip1);
+var b09 = [
+	[0, 3],
+	[3, 6],
+	[6, 9],
+	[9, 12]
+].map(function(n) {
+	return new VF.Beam(m09.slice(n[0], n[1]));
+});
+var t09 = [
+	[0,3],
+	[3,6],
+	[6,9],
+	[9,12]
+].map(function(n) {
+	return new VF.Tuplet(m09.slice(n[0], n[1]));
+});
+// var simpleTriplet = new Vex.Flow.Tuplet(notes.slice(14,17));
+VF.Formatter.FormatAndDraw(context9, stave09, m09);
+b09.forEach(function(b) {b.setContext(context9).draw()});
+t09.forEach(function(t) {t.setContext(context9).draw()});
 
 
 var btn = document.querySelector("#generate");
@@ -166,3 +208,14 @@ var svg = document.querySelectorAll('svg');
 svg.forEach(function(f){
 	f.setAttribute("viewBox", `0 0 ${MEASURE_WIDTH} ${MEASURE_HEIGHT}`);
 })
+
+document.querySelector('div #measure9 svg').setAttribute("viewBox", `0 0 ${MEAS_WIDTH_TRIP} ${MEASURE_HEIGHT}`);
+
+// Pass in querySelectorAll of measures.
+function deleteBar(q) {
+	for (i = 0; i < q.length; i++) {
+		while (q[i].hasChildNodes()) {
+			q[i].removeChild(q[i].lastChild);
+		}
+	}
+}
